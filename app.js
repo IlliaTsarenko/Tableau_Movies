fe169fe4838a13dac73485bcbe0d5da67296ebb3
Illia Tsarenko <154458335+IlliaTsarenko@users.noreply.github.com>
2023-12-26 16:10:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
-console.log('Is this working?');
-
-let viz;
-
-//Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/Movies_16890041307980/Dashboard1?:language=en-GB&:display_count=n&:origin=viz_share_link";
-
-const vizContainer = document.getElementById('vizContainer');
-const options = {
-    hideTabs: true,
-    height: 1000,
-    width: 1200,
-    onFirstInteraction: function() {
-        workbook = viz.getWorkbook();
-        activeSheet = workbook.getActiveSheet();
-        console.log("My dashboard is interactive");
-    }
-};
-
-//create a function to generate the viz element
-function initViz() {
-    console.log('Executing the initViz function!');
-    viz = new tableau.Viz(vizContainer, url, options);
-}
-
-// run the initViz function when the page loads
-document.addEventListener("DOMContentLoaded", initViz);
-
-const exportPDF = document.getElementById('exportPDF');
-const exportImage = document.getElementById('exportImage');
-
-
-//click on the pdf button to generate pdf of dashboard
-function generatePDF() {
-    viz.showExportPDFDialog()
-}
-
-exportPDF.addEventListener("click", function () {
-    generatePDF();
-  });
-
-//click on image to generate image of dashboard
-function generateImage() {
-    viz.showExportImageDialog()
-}
-
-exportImage.addEventListener("click", function () {
-    generateImage();
-  });
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Movies_16890041307980</title>
+</head>
+<body>
+  <h1>Movies_16890041307980</h1>
+  <button id="exportPDF">Generate PDF</button>
+  <button id="exportImage">Generate Image</button>
+  <div id="vizContainer"></div>
+  <script src="./EmbedTableauViz.js"></script>
+</body>
+</html>
